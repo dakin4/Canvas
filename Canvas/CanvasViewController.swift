f9b78c6e39b2c091ac8b4192334921f31e66ced2
dakin4 <davekng0001@gmail.com>
2018-03-31 11:38:45 -0400

    Required Stories

--- Canvas/CanvasViewController.swift
+++ Canvas/CanvasViewController.swift
@@ -12,7 +12,7 @@ import UIKit
 
 class CanvasViewController: UIViewController {
 
-    
+    var newlyCreatedFace: UIImageView!
     
     @IBOutlet weak var UIViewTray: UIView!
     
@@ -23,12 +23,216 @@ class CanvasViewController: UIViewController {
     var trayUp: CGPoint?
     
     var trayDown: CGPoint?
+   
+    var newlyCreatedFaceOrigCenter: CGPoint!
+    
+    //var panGesture:UIPanGestureRecognizer?
+    
+    var newEmojiCenter: CGPoint?
+    var NewFace: UIImageView!
+     //var NewFace: [UIImageView] = []
+    
+    @IBAction func DeadPanGesture(_ sender: UIPanGestureRecognizer) {
+    
+    var translation = sender.translation(in: view)
+    
+        var imageDead = sender.view as! UIImageView
+       
+        
+        
+        print("deadpan")
+       
+        switch sender.state
+        {
+        case .began:
+        print("dead emoji began")
+        began(Image: imageDead)
+        
+            
+        case .changed:
+        print("dead emoji changed")
+        newlyCreatedFace.center = CGPoint(x: newlyCreatedFaceOrigCenter.x + translation.x, y: newlyCreatedFaceOrigCenter.y + translation.y)
+           
+            
+        case .ended:
+        print("dead emoji ended")
+         let panGesture = UIPanGestureRecognizer(target: self, action: #selector(NewEmojiPan(sender:)))
+        newlyCreatedFace.addGestureRecognizer(panGesture)
+        newlyCreatedFace.isUserInteractionEnabled = true
+        default:
+        return
+        
+        }
+      
+        
+        
+    }
+    
+   
+    
+    @IBAction func ExcitedPanGesture(_ sender: UIPanGestureRecognizer) {
+        
+        var translation = sender.translation(in: view)
+        
+        var imageExcited = sender.view as! UIImageView
+        
+        switch sender.state
+        {
+        case .began:
+            print("excited emoji began")
+            began(Image: imageExcited)
+            
+            
+            
+        case .changed:
+            print("excited emoji changed")
+             newlyCreatedFace.center = CGPoint(x: newlyCreatedFaceOrigCenter.x + translation.x, y: newlyCreatedFaceOrigCenter.y + translation.y)
+            
+        case .ended:
+            print("excited emoji ended")
+            let panGesture = UIPanGestureRecognizer(target: self, action: #selector(NewEmojiPan(sender:)))
+            newlyCreatedFace.addGestureRecognizer(panGesture)
+            newlyCreatedFace.isUserInteractionEnabled = true
+            newEmojiCenter = newlyCreatedFace.center
+            
+        default:
+            return
+            
+        }
+
+    }
+    
+    @IBAction func HappyPanGesture(_ sender: UIPanGestureRecognizer) {
+        var translation = sender.translation(in: view)
+        
+        var imageHappy = sender.view as! UIImageView
+        
+        switch sender.state
+        {
+        case .began:
+            print("happy emoji began")
+            began(Image: imageHappy)
+            
+            
+        case .changed:
+            print("happy emoji changed")
+             newlyCreatedFace.center = CGPoint(x: newlyCreatedFaceOrigCenter.x + translation.x, y: newlyCreatedFaceOrigCenter.y + translation.y)
+            
+        case .ended:
+            print("happy emoji ended")
+            let panGesture = UIPanGestureRecognizer(target: self, action: #selector(NewEmojiPan(sender:)))
+            newlyCreatedFace.addGestureRecognizer(panGesture)
+            newlyCreatedFace.isUserInteractionEnabled = true
+            newEmojiCenter = newlyCreatedFace.center
+            
+        default:
+            return
+            
+        }
+
+    }
+    
+    
+    @IBAction func SadPanGesture(_ sender: UIPanGestureRecognizer) {
+        var translation = sender.translation(in: view)
+        
+        var imageSad = sender.view as! UIImageView
+        
+        switch sender.state
+        {
+        case .began:
+            print("Sad emoji began")
+            began(Image: imageSad)
+            
+            
+        case .changed:
+            print("Sad emoji changed")
+               newlyCreatedFace.center = CGPoint(x: newlyCreatedFaceOrigCenter.x + translation.x, y: newlyCreatedFaceOrigCenter.y + translation.y)
+            
+        case .ended:
+            print("Sad emoji ended")
+            let panGesture = UIPanGestureRecognizer(target: self, action: #selector(NewEmojiPan(sender:)))
+            newlyCreatedFace.addGestureRecognizer(panGesture)
+            newlyCreatedFace.isUserInteractionEnabled = true
+            newEmojiCenter = newlyCreatedFace.center
+            
+        default:
+            return
+            
+        }
+
+    }
+    
+    @IBAction func TonguePanGesture(_ sender: UIPanGestureRecognizer) {
+        
+        var translation = sender.translation(in: view)
+        
+        var imageTongue = sender.view as! UIImageView
+        switch sender.state
+        {
+        case .began:
+            print("Tongue emoji began")
+             began(Image: imageTongue)
+            
+            
+        case .changed:
+            print("Tongue emoji changed")
+            newlyCreatedFace.center = CGPoint(x: newlyCreatedFaceOrigCenter.x + translation.x, y: newlyCreatedFaceOrigCenter.y + translation.y)
+            
+            
+        case .ended:
+            print("Tongue emoji ended")
+            let panGesture = UIPanGestureRecognizer(target: self, action: #selector(NewEmojiPan(sender:)))
+            newlyCreatedFace.addGestureRecognizer(panGesture)
+            newlyCreatedFace.isUserInteractionEnabled = true
+            newEmojiCenter = newlyCreatedFace.center
+            
+        default:
+            return
+            
+        }
+
+    }
+    
+    
+    @IBAction func WinkPanGesture(_ sender: UIPanGestureRecognizer) {
+        var translation = sender.translation(in: view)
+        
+        var imageWink = sender.view as! UIImageView
+        
+        switch sender.state
+        {
+        case .began:
+            print("wink emoji began")
+            began(Image: imageWink)
+            
+            
+        case .changed:
+            print("wink emoji changed")
+              newlyCreatedFace.center = CGPoint(x: newlyCreatedFaceOrigCenter.x + translation.x, y: newlyCreatedFaceOrigCenter.y + translation.y)
+            
+        case .ended:
+            print("wink emoji ended")
+            let panGesture = UIPanGestureRecognizer(target: self, action: #selector(NewEmojiPan(sender:)))
+            newlyCreatedFace.addGestureRecognizer(panGesture)
+            newlyCreatedFace.isUserInteractionEnabled = true
+            newEmojiCenter = newlyCreatedFace.center
+            
+            
+        default:
+            return
+            
+        }
+
+    }
+    
+    
     
     
     @IBAction func TrayPanGesture(_ sender: UIPanGestureRecognizer) {
         
     
-   trayUp = UIViewTray.center
+
         
     var velocity = sender.velocity(in: view)
     
@@ -45,31 +249,92 @@ class CanvasViewController: UIViewController {
             
         }
        else if (sender.state == .changed){
-                    UIViewTray.center = CGPoint(x: trayOriginalCenter!.x, y: trayOriginalCenter!.y + translation.y )
+                    if (UIViewTray.center.y > trayUp!.y && UIViewTray.center.y < trayDown!.y || UIViewTray.center.y == trayUp!.y && translation.y > 0 || UIViewTray.center.y == trayDown!.y && translation.y < 0)
+                    {
+            
+                       
+                        
+            UIViewTray.center = CGPoint(x: trayOriginalCenter!.x, y: trayOriginalCenter!.y + translation.y )
             print("translation y \(translation.y)")
+            }
+           // print ("end changed")
            
         }
             
        else if (sender.state == .ended){
             if (velocity.y > 0){
-                UIView.animate(withDuration: 0.1, animations: { 
+           
+                UIView.animate(withDuration: 0.1, delay: 0, usingSpringWithDamping: 0.4, initialSpringVelocity: 1, options: [], animations: {
+                    
+                    
                     self.UIViewTray.center = self.trayDown!
-                })
+                    
+                    }, completion: nil)
+                
                 
             }
            else if (velocity.y < 0){
-                print("less \(self.UIViewTray.center)")
-                UIView.animate(withDuration: 0.6, animations: {
-                   
-                    self.UIViewTray.center = self.trayUp!
-                    
-                })
+               UIView.animate(withDuration: 0.07, delay: 0, usingSpringWithDamping: 0.4, initialSpringVelocity: 1, options: [], animations: {
                 
+                
+                self.UIViewTray.center = self.trayUp!
+                
+                }, completion: nil)
+       
+               print("less \(self.UIViewTray.center)")
             }
         }
-        
-        
     }
+
+    
+    
+    
+    func began (Image: UIImageView) {
+        newlyCreatedFace = UIImageView(image: Image.image)
+        view.addSubview(newlyCreatedFace)
+        newlyCreatedFace.center = Image.center
+        newlyCreatedFace.center.y += UIViewTray.frame.origin.y
+        newlyCreatedFaceOrigCenter = newlyCreatedFace.center
+    }
+    
+  
+    func NewEmojiPan(sender: UIPanGestureRecognizer) {
+        
+        var Emoji = sender.view as! UIImageView
+       
+        var translation = sender.translation(in: view)
+        print("new emoji \(translation)")
+        
+        
+        
+        
+        switch sender.state
+        {
+        case .began:
+            print("Created Pan began")
+            newEmojiCenter = Emoji.center
+            Emoji.center = CGPoint(x: newEmojiCenter!.x + translation.x, y: newEmojiCenter!.y + translation.y)
+            
+            
+            
+            
+        case .changed:
+            print("Created emoji changed")
+            Emoji.center = CGPoint(x: newEmojiCenter!.x + translation.x, y: newEmojiCenter!.y + translation.y)
+            
+            
+        case .ended:
+            print("Created emoji ended")
+            newEmojiCenter = Emoji.center
+            
+        default:
+            return
+            
+        }
+
+    }
+    
+    
     
     
     
@@ -80,17 +345,13 @@ class CanvasViewController: UIViewController {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
         
-        trayDownOffset = 250
-        trayUp = UIViewTray.center
-        print("tray up \(trayUp)")
+        trayDownOffset = 130
+        // brings tray down to look better
+        
+        trayUp = CGPoint(x: UIViewTray.center.x, y: UIViewTray.center.y + 60)
+        UIViewTray.center = trayUp!
+        print("view \(UIViewTray.center.y)")
         trayDown = CGPoint(x: UIViewTray.center.x, y: UIViewTray.center.y + trayDownOffset!)
-        
-      
-        
-        
-        
-        
-        
         
         
     }
